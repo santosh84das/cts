c07ae8a36b211ea307ea6ee1b442492fdc16bb2a
Sunanda Ghosh <66545585+sunandaghosh@users.noreply.github.com>
2024-04-06 17:46:20 +0530

    bankdetails

---
+++ src/app/core/models/bank.ts
@@ -0,0 +1,5 @@
+export interface bankDetails {
+    branchName: string, 
+    mircCode: string,
+}
+
--- src/app/features/cheque/cheque-indent-invoice/cheque-indent/cheque-indent.component.ts
+++ src/app/features/cheque/cheque-indent-invoice/cheque-indent/cheque-indent.component.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { IapiResponce } from 'src/app/core/models/iapi-responce';
-
+import { BankService } from 'src/app/core/services/Bank/bank.service';
+import { ToastService } from 'src/app/core/services/toast.service';
 interface Chequetype {
   name: string;
   code: Number;
@@ -15,15 +14,22 @@ interface Chequetype {
   styleUrls: ['./cheque-indent.component.scss']
 })
 export class ChequeIndentComponent implements OnInit {
+  [x: string]: any;
 
   cheques!: Chequetype[];
   indentForm!: FormGroup;
   lastindex!: number;
   displayModal: boolean | undefined;
   toastservice: any;
+  bankList: [] = [];
+  branchlist: any[] = [];
+  selectedBank: any;
+  selectedBranch: any;
+  bankDetails: any[] = [];
+  selectedBranchName: any;
 
 
-  constructor(private _fb: FormBuilder, private http: HttpClient) { }
+  constructor(private _fb: FormBuilder, private bankServices: BankService, private toastService: ToastService) { }
 
   ngOnInit(): void {
     this.cheques = [
@@ -33,7 +39,7 @@ export class ChequeIndentComponent implements OnInit {
     this.indentForm = this._fb.group({
       chequelist: this._fb.array([this.createCheque()])
     });
-
+    this.getBanklist()
   }
 
   get chequelist(): FormArray {
@@ -61,11 +67,49 @@ export class ChequeIndentComponent implements OnInit {
     this.displayModal = true;
   }
 
-  // getBankList(): Observable<IapiResponce> {
-  //   return this.http.post<IapiResponce>('v1/Bank/get-banks').pipe(
+  getBanklist() {
+    this.bankServices.getBankList().subscribe((response) => {
+      if (response.apiResponseStatus == 1) {
+        this.bankList = response.result
+        console.log('hi', this.bankList);
 
-  //   );
+      } else {
+        this.toastService.showAlert(
+          response.message,
+          response.apiResponseStatus
+        )
+      }
+    })
+  }
+  getBranchList() {
+    console.log(this.selectedBank);
+
+    this.bankServices.getBankBranches(this.selectedBank.code).subscribe((response) => {
+      if (response.apiResponseStatus == 1) {
+        this.branchlist = response.result
+      } else {
+        this.toastService.showAlert(
+          response.message,
+          response.apiResponseStatus
+        )
+      }
+    })
+  }
+
+  // getBranchDeatils() {
+  //   if (this.selectedBranchName) {
+  //     this.bankServices.getBranchDetail(this.selectedBranch.code).subscribe((response) => {
+  //       if (response.apiResponseStatus == 1) {
+  //         this.bankDetails = response.result
+  //       } else {
+  //         this.toastService.showAlert(
+  //           response.message,
+  //           response.apiResponseStatus
+  //         )
+  //       }
+  //     })
+  //   }
+
   // }
-
 
 }
