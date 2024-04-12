a4f16accefa811924e32a928914292ace9745c6e
Sunanda Ghosh <66545585+sunandaghosh@users.noreply.github.com>
2024-04-12 16:33:05 +0530

    update payment mandate

--- src/app/features/payment-mandate/pay-mandate-shortlist/pay-mandate-shortlist.component.ts
+++ src/app/features/payment-mandate/pay-mandate-shortlist/pay-mandate-shortlist.component.ts
@@ -12,12 +12,13 @@ import { BillService } from 'src/app/core/services/Bill/bill.service';
   styleUrls: ['./pay-mandate-shortlist.component.scss']
 })
 export class PayMandateShortlistComponent implements OnInit {
+[x: string]: any;
   stateOptions: any[] = [{ label: 'Current Financial Year', value: '1' }, { label: 'Previous Financial Year  ', value: '2' }];
   value: any = '1';
   isPaymentSelected: boolean = true;
   loading: boolean = false;
   paymandateShortlist: Paymandate[] = [];
-  selectedPaymandates: Paymandate[] = [];
+  selectedPaymandates: any;
   constructor(private paymandateservice: PaymandateService, public billservice: BillService,) { }
 
   ngOnInit(): void {
@@ -37,34 +38,38 @@ export class PayMandateShortlistComponent implements OnInit {
   }
 
   selectPaymandate(paymandate: any) {
-    if (!this.selectedPaymandates) {
-      this.selectedPaymandates = []; 
-    }
-    this.selectedPaymandates.push(paymandate);
+    // console.log(paymandate);
+    
+    // if (!this.selectedPaymandates) {
+    //   this.selectedPaymandates = []; 
+    // }
+    // this.selectedPaymandates.push(paymandate);
   }
 
   updateSelectedPaymandates(paymandate: any) {
-    if (!this.selectedPaymandates) {
-      this.selectedPaymandates = []; 
-    }
-    if (paymandate.selected) {
-      this.selectedPaymandates.push(paymandate);
-    } else {
-      const index = this.selectedPaymandates.findIndex(item => item.tokenId === paymandate.tokenId);
-      if (index !== -1) {
-        this.selectedPaymandates.splice(index, 1);
-      }
-    }
+    // console.log(paymandate);
+    
+    // if (!this.selectedPaymandates) {
+    //   this.selectedPaymandates = []; 
+    // }
+    // if (paymandate.selected) {
+    //   this.selectedPaymandates.push(paymandate);
+    // } else {
+    //   const index = this.selectedPaymandates.findIndex(item => item?.tokenId === paymandate?.tokenId);
+    //   if (index !== -1) {
+    //     this.selectedPaymandates.splice(index, 1);
+    //   }
+    // }
   }
+  
 
   dataSave() {
     if (this.selectedPaymandates && this.selectedPaymandates.length > 0) {
-      console.log(this.selectedPaymandates);
-      const payload = {
-        tokenId: this.selectedPaymandates[0].tokenId,
-        calendarDate: this.selectedPaymandates[0].selectedDate,
-      };
-      console.log(payload);
+      const payloadArray = this.selectedPaymandates.map((paymandate: { tokenId: any; selectedDate: any; }) => ({
+          tokenId: paymandate.tokenId,
+          calendarDate: paymandate.selectedDate,
+      }));
+      console.log('---->',payloadArray);
     } else {
       console.error('No paymandates selected.');
     }
