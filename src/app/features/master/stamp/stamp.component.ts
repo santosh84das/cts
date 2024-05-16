b365fb84a47aafa4a1ac0b3d8207a5cccc4ea0dd
Pritam Sarkar <sarkarpritam0007@gmail.com>
2024-05-17 03:31:11 +0530

    stamp component added under master component with its sub-componets

---
+++ src/app/features/master/stamp/stamp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stamp',
+  templateUrl: './stamp.component.html',
+  styleUrls: ['./stamp.component.scss']
+})
+export class StampComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/features/master/stamp/stamp.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StampRoutingModule } from './stamp-routing.module';
+import { StampComponent } from './stamp.component';
+import { LabelComponent } from './label/label.component';
+import { OptionCardModule } from 'src/app/shared/modules/option-card/option-card.module';
+import { CommonHeaderModule } from 'src/app/shared/modules/common-header/common-header.module';
+import { ButtonModule } from 'primeng/button';
+import { DynamicTableModule } from 'src/app/shared/modules/dynamic-table/dynamic-table.module';
+import { CategoryComponent } from './category/category.component';
+import { TypeComponent } from './type/type.component';
+import { VendorComponent } from './vendor/vendor.component';
+
+
+@NgModule({
+  declarations: [
+    StampComponent,
+    LabelComponent,
+    CategoryComponent,
+    TypeComponent,
+    VendorComponent
+  ],
+  imports: [
+    CommonModule,
+    StampRoutingModule,
+    OptionCardModule,
+    ButtonModule,
+    DynamicTableModule,
+    CommonHeaderModule
+  ]
+})
+export class StampModule { }
