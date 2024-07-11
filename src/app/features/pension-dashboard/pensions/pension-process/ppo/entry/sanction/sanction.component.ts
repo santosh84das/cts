b0beb45864a4f03c5f0ff8f6f4ffe0662ad64e7d
Nilakshi Chakraborty <chakraborty.nilakshi2004@gmail.com>
2024-07-11 15:13:16 +0530

    sanction-form-structure

--- src/app/features/pension-dashboard/pensions/pension-process/ppo/entry/sanction/sanction.component.ts
+++ src/app/features/pension-dashboard/pensions/pension-process/ppo/entry/sanction/sanction.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SelectItem } from 'primeng/api';
 
 
 @Component({
@@ -10,6 +11,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SanctionComponent implements OnInit {
 
   ppoSanctionForm!: FormGroup;
+  genders: SelectItem[]=[]
+  selectedDrop: SelectItem = { value: '' };
 
   constructor(private fb: FormBuilder) {
 
@@ -17,18 +20,44 @@ export class SanctionComponent implements OnInit {
 
   ngOnInit(): void {
     this.initializer();
+
+    this.genders = [
+      { label: 'Male', value: { id: 1, name: 'Male', code: 'M' } },
+      { label: 'Female', value: { id: 2, name: 'Female', code: 'F' } },
+      { label: 'Transgender', value: { id: 3, name: 'Transgender', code: 'T' } },
+  ];
   }
 
   initializer(): void {
     this.ppoSanctionForm = this.fb.group({
-      transferredPpoNo: ['', Validators.required],
-      ePpoSearchDetails: ['', Validators.required]
+      PpoId: ['', Validators.required],
+      nameOfServiceHolder: ['', Validators.required],
+      sanctionAuthority: ['', Validators.required],
+      sanctionNo: ['', Validators.required],
+      sanctionDate: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      gender: ['',Validators.required],
+      dateOfAppointment: ['', Validators.required],
+      officeName: ['', Validators.required],
+      postHeld: ['',Validators.required],
+      lastPay: ['',Validators.required],
+      averageAmolument:['',Validators.required],
+      hrmsUniqueIdOfServiceHolder: ['',Validators.required],
+      issuingAuthority: ['',Validators.required],
+      letterNo: ['',Validators.required],
+      letterDate: ['',Validators.required],
+      grossYear: ['',Validators.required],
+      grossMonth: ['',Validators.required],
+      grossDay: ['',Validators.required],
+      netYear: ['',Validators.required],
+      netMonth: ['',Validators.required],
+      netDay: ['',Validators.required],
     });
   }
 
   onSearch(): void {
     if (this.ppoSanctionForm.valid) {
-      console.log(this.ppoSanctionForm.value); // Replace with your actual search logic
+      console.log(this.ppoSanctionForm.value); 
     }
   }
 }
