813939a46d7a0433fac2ccb3d2c817392d7244af
Pritam Sarkar <sarkarpritam0007@gmail.com>
2024-07-17 01:38:34 +0530

    Removed multiple row selection from the datatable

--- src/app/features/stamp-strong-room/reports/reports.module.ts
+++ src/app/features/stamp-strong-room/reports/reports.module.ts
@@ -4,12 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports.component';
 import { OptionCardModule } from 'src/app/shared/modules/option-card/option-card.module';
-import { VendorWiseEC136Component } from './tds-details/vendor-wise-ec136/vendor-wise-ec136.component';
-import { VendorDetailsWiseEC137Component } from './tds-details/vendor-details-wise-ec137/vendor-details-wise-ec137.component';
 
 
 @NgModule({
-  declarations: [ReportsComponent, VendorWiseEC136Component, VendorDetailsWiseEC137Component],
+  declarations: [ReportsComponent],
   imports: [
     CommonModule,
     OptionCardModule,
--- src/app/features/stamp-strong-room/reports/tds-details/tds-details.module.ts
+++ src/app/features/stamp-strong-room/reports/tds-details/tds-details.module.ts
@@ -6,14 +6,15 @@ import { OptionCardModule } from 'src/app/shared/modules/option-card/option-card
 import { TdsDetailsComponent } from './tds-details.component';
 import { VendorDetailsWiseEC137Component } from './vendor-details-wise-ec137/vendor-details-wise-ec137.component';
 import { VendorWiseEC136Component } from './vendor-wise-ec136/vendor-wise-ec136.component';
+import { OptionCardComponent } from 'src/app/shared/modules/option-card/option-card.component';
 
 
 @NgModule({
   declarations: [TdsDetailsComponent, VendorDetailsWiseEC137Component, VendorWiseEC136Component],
   imports: [
     CommonModule,
-    OptionCardModule,
+    // OptionCardModule,
     TdsDetailsRoutingModule
-  ]
+  ],
 })
 export class TdsDetailsModule { }
