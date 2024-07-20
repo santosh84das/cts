020621af0e55644c137e28e706b1fbdc2199b93a
Amit Maity <amitmaity2024@gmail.com>
2024-07-21 00:10:12 +0530

    sss

--- src/app/features/pension-dashboard/pensions/pension-process/pension-bill/pension-bill-routing.module.ts
+++ src/app/features/pension-dashboard/pensions/pension-process/pension-bill/pension-bill-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PensionBillComponent } from './pension-bill.component';
+// import { PensionBillComponent } from './pension-bill.component';
 import {PentionDetailComponent} from './../pention-detail/pention-detail.component'
+import { BillDetailComponent } from '../bill-detail/bill-detail.component';
 
 const routes: Routes = [
-    {path:'', component: PensionBillComponent},
+    {path:'', component: BillDetailComponent},
     {path:'pention-detail', component: PentionDetailComponent}
 ];
 
--- src/app/features/pension-dashboard/pensions/pension-process/pension-bill/pension-bill.component.ts
+++ src/app/features/pension-dashboard/pensions/pension-process/pension-bill/pension-bill.component.ts
@@ -1,28 +1,87 @@
-import { style } from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
-import { PentionDetailComponent } from './../pention-detail/pention-detail.component';
-import { MenuItem } from 'primeng/api';
-import {PensionProcessComponent} from './../pension-process.component';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { Product } from 'src/app/demo/api/product';
+import { ProductService } from 'src/app/demo/service/product.service';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-pension-bill',
   templateUrl: 'pension-bill.component.html',
+  providers: [MessageService, ConfirmationService],
   styles: [`:host ::ng-deep .p-menubar-root-list {
     flex-wrap: wrap;
 }`]
 })
 export class PensionBillComponent implements OnInit {
+  steps: any[];
+  currentStepIndex: number = 0;
+  dropdownItems: { name: string; code: string; }[];
+  selectedMode: any = null;
+  statuses: any[] = [];
 
-  constructor() { }
-  routeItems: MenuItem[] = [];
-  ngOnInit(): void {
-    this.routeItems = [
-        { label: 'Pension Details', routerLink: 'app-persion-detail' },
-        { label: 'Seat', routerLink: 'seat' },
-        { label: 'Payment', routerLink: 'payment' },
-        { label: 'Confirmation', routerLink: 'confirmation' },
+  products: Product[] = [];
+
+  rowGroupMetadata: any;
+
+  expandedRows: { [key: string]: boolean } = {};
+
+  isExpanded: boolean = false;
+
+  @ViewChild('filter') filter!: ElementRef;
+
+  constructor(private router: Router, private productService: ProductService) {
+    this.steps = [
+      { label: 'Pension Details' },
+      { label: 'Bill Details' },
+      { label: 'Component Details' }
+    ];
+    this.dropdownItems = [
+      { name: 'Treasury', code: 'Treasury' },
+      { name: 'Option 2', code: 'Option 2' },
+      { name: 'Option 3', code: 'Option 3' }
     ];
   }
 
+  ngOnInit(): void {
+    this.productService
+      .getProductsWithOrdersSmall()
+      .then((data) => (this.products = data));
+  }
+
+  next() {
+    this.currentStepIndex++;
+  }
+
+  prev() {
+    if (this.currentStepIndex > 0) {
+      this.currentStepIndex--;
+    }
+  }
+  save(){}
+
+  expandAll() {
+    if (!this.isExpanded) {
+      this.products.forEach((product) =>
+        product && product.name
+          ? (this.expandedRows[product.name] = true)
+          : ''
+      );
+    } else {
+      this.expandedRows = {};
+    }
+    this.isExpanded = !this.isExpanded;
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal(
+      (event.target as HTMLInputElement).value,
+      'contains'
+    );
+  }
+
+  clear(table: Table) {
+    table.clear();
+    this.filter.nativeElement.value = '';
+  }
 }
