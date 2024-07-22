9759928b3f3c120803fedfb6a86636ecda28cfbd
shruti <shruti040112@gmail.com>
2024-07-22 16:19:31 +0530

    convert to family pension

--- src/app/features/pension-dashboard/pensions/pension-process/pension-process.module.ts
+++ src/app/features/pension-dashboard/pensions/pension-process/pension-process.module.ts
@@ -1,22 +1,55 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { ButtonModule } from 'primeng/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
+import { AccordionModule } from 'primeng/accordion';
+import { AutoCompleteModule } from "primeng/autocomplete";
+import { CalendarModule } from 'primeng/calendar';
+import { CascadeSelectModule } from "primeng/cascadeselect";
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipModule } from "primeng/chip";
+import { ChipsModule } from "primeng/chips";
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from 'primeng/divider';
+import { DropdownModule } from 'primeng/dropdown';
+import { FieldsetModule } from 'primeng/fieldset';
+import { InputMaskModule } from "primeng/inputmask";
+import { InputNumberModule } from "primeng/inputnumber";
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from "primeng/inputtext";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { KnobModule } from 'primeng/knob';
+import { ListboxModule } from 'primeng/listbox';
+import { MenuModule } from 'primeng/menu';
+import { MultiSelectModule } from "primeng/multiselect";
+import { PanelModule } from 'primeng/panel';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RatingModule } from 'primeng/rating';
+import { RippleModule } from 'primeng/ripple';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SliderModule } from 'primeng/slider';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { SplitterModule } from 'primeng/splitter';
+import { TableModule } from 'primeng/table';
+import { TabViewModule } from 'primeng/tabview';
+import { ToastModule } from 'primeng/toast';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { CommonHeaderModule } from 'src/app/shared/modules/common-header/common-header.module';
 import { DynamicTableModule } from 'src/app/shared/modules/dynamic-table/dynamic-table.module';
 import { OptionCardModule } from 'src/app/shared/modules/option-card/option-card.module';
-import { CommonHeaderModule } from 'src/app/shared/modules/common-header/common-header.module';
-import { DropdownModule } from 'primeng/dropdown';
-import { DialogModule } from 'primeng/dialog';
-import { CalendarModule } from 'primeng/calendar';
 import { TreasuryDropdownModule } from 'src/app/shared/modules/treasury-dropdown/treasury-dropdown.module';
-import { PensionProcessComponent } from './pension-process.component';
+import { ConvertToFamilyPensionComponent } from './convert-to-family-pension/convert-to-family-pension.component';
 import { PensionProcessRoutingModule } from './pension-process-routing.module';
+import { PensionProcessComponent } from './pension-process.component';
 
 @NgModule({
   declarations: [
-    PensionProcessComponent
+    PensionProcessComponent,
+    ConvertToFamilyPensionComponent
   ],
   imports: [
     CommonModule,
@@ -30,7 +63,39 @@ import { PensionProcessRoutingModule } from './pension-process-routing.module';
     CalendarModule,
     TreasuryDropdownModule,
     FormsModule,
-    PensionProcessRoutingModule
+    PensionProcessRoutingModule,
+    AutoCompleteModule,
+    ChipsModule,
+    InputMaskModule,
+    InputNumberModule,
+    CascadeSelectModule,
+    MultiSelectModule,
+    InputTextareaModule,
+    InputTextModule,
+    RatingModule,
+    ChipModule,
+    KnobModule,
+    ListboxModule,
+    SelectButtonModule,
+    CheckboxModule,
+    InputSwitchModule,
+    RadioButtonModule,
+    ColorPickerModule,
+    ToggleButtonModule,
+    SliderModule,
+    RippleModule,
+    SplitButtonModule,
+    AccordionModule,
+    TabViewModule,
+    FieldsetModule,
+    MenuModule,
+    DividerModule,
+    SplitterModule,
+    PanelModule,
+    ToastModule,
+    ProgressBarModule,
+    TableModule,
+    CalendarModule,
   ]
 })
 export class PensionProcessModule { }
