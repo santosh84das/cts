ac6028fca9d441469b7579efa269e4d8794bac7c
shruti <shruti040112@gmail.com>
2024-07-22 22:49:09 +0530

    convert-to-family-pension

---
+++ src/app/features/pension-dashboard/pensions/pension-process/convert-to-family-pension/convert-to-family-pension.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ConvertToFamilyPensionComponent } from './convert-to-family-pension.component';
+
+
+
+@NgModule({
+  declarations: [ConvertToFamilyPensionComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConvertToFamilyPensionModule { }
