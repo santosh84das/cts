48d5f9e5e595e5b1ce00620fdfb51bd9c2d760a0
kirito <roy184433@gmail.com>
2024-07-22 10:40:20 +0530

    submit button added

--- src/app/features/pension-dashboard/pensions/pension-process/pension-process-routing.module.ts
+++ src/app/features/pension-dashboard/pensions/pension-process/pension-process-routing.module.ts
@@ -4,21 +4,12 @@ import { PensionProcessComponent } from './pension-process.component';
 import { PpoComponent } from './ppo/ppo.component';
 import { EntryComponent } from './ppo/entry/entry.component';
 import { PensionBillComponent } from './pension-bill/pension-bill.component';
-import { PentionDetailComponent } from './pention-detail/pention-detail.component';
-import { BillDetailComponent } from './bill-detail/bill-detail.component';
-import { ClassificationDetailsComponent } from './classification-details/classification-details.component';
-import { ComponentDetailsComponent } from './component-details/component-details.component';
+
 const routes: Routes = [
     { path: '', component: PensionProcessComponent },
     { path: 'ppo', component: PpoComponent },
     { path: 'app-pension-billS', component: PensionBillComponent },
-    { path: 'app-pension-detail', component: PentionDetailComponent },
-    { path: 'app-bill-detail', component: BillDetailComponent },
-    {
-        path: 'app-classification-details',
-        component: ClassificationDetailsComponent,
-    },
-    { path: 'app-component-details', component: ComponentDetailsComponent },
+
 ];
 
 @NgModule({
