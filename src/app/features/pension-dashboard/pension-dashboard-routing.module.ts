13443564c6a1340788ff94bd6bfbe56ae256dae7
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-07-25 15:38:43 +0530

    Fix errors

---
+++ src/app/features/pension-dashboard/pension-dashboard-routing.module.ts
@@ -0,0 +1,70 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PensionDashboardComponent } from './pension-dashboard.component';
+
+
+const routes: Routes = [
+  { path: '', component: PensionDashboardComponent },
+  {
+    path: 'modules',
+    loadChildren: () => import('./pensions/pensions.module').then(m => m.PensionsModule),
+  },
+  {
+    path: 'modules/pension-process',
+    loadChildren: () =>
+      import('./pensions/pension-process/pension-process.module').then(m => m.PensionProcessModule),
+  },
+  {
+    path: 'modules/pension-process/ppo',
+    loadChildren: () => import('./pensions/pension-process/ppo/ppo.module').then(m => m.PpoModule),
+  },
+  {
+    path: 'modules/pension-process/ppo/entry',
+    loadChildren: () => import('./pensions/pension-process/ppo/entry/entry.module').then(m => m.EntryModule),
+  },
+  {
+    path: 'modules/pension-process/ppo/entry/ppodetails',
+    loadChildren: () =>
+      import('./pensions/pension-process/ppo/entry/ppodetails/ppodetails.module').then(m => m.PpodetailsModule),
+  },
+  {
+    path: 'modules/pension-process/ppo/manual-ppo-receipt',
+    loadChildren: () =>
+      import('./pensions/pension-process/ppo/manual-ppo-receipt/manual-ppo-receipt.module').then(m => m.ManualPpoReceiptModule),
+  },
+  {
+    path: 'modules/pension-process/ppo/entry/sanction',
+    loadChildren: () => import('./pensions/pension-process/ppo/entry/ppodetails/sanction/sanction.module').then(m => m.SanctionModule),
+  },
+  {
+    path: 'modules/pension-process/ppo/entry/family-nominee',
+    loadChildren: () =>
+      import('./pensions/pension-process/ppo/entry/ppodetails/family-nominee/family-nominee.module').then(m => m.FamilyNomineeModule),
+  },
+  {
+   path: 'modules/pension-process/pension-bill',
+   loadChildren: () => import('./pensions/pension-process/pension-bill/pension-bill.module').then(m => m.PensionBillModule),
+  },
+  {
+    path: 'modules/pension-process/ppo/convert-to-family-pension',
+    loadChildren: () => import('./pensions/pension-process/ppo/convert-to-family-pension/convert-to-family-pension.module').then(m => m.ConvertToFamilyPensionModule),
+   },
+   {
+    path: 'modules/pension-process/ppo/life-certificate',
+    loadChildren: () => import('./pensions/pension-process/ppo/life-certificate/life-certificate.module').then(m => m.LifeCertificateModule),
+   }
+  
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes),
+  ],
+  exports: [RouterModule],
+})
+export class PensionDashboardRoutingModule {}
---
+++ src/app/features/pension-dashboard/pensions/pension-process/ppo/ppo.module.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+import { CalendarModule } from 'primeng/calendar';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { CommonHeaderModule } from 'src/app/shared/modules/common-header/common-header.module';
+import { DynamicTableModule } from 'src/app/shared/modules/dynamic-table/dynamic-table.module';
+import { OptionCardModule } from 'src/app/shared/modules/option-card/option-card.module';
+import { TreasuryDropdownModule } from 'src/app/shared/modules/treasury-dropdown/treasury-dropdown.module';
+import { EntryComponent } from './entry/entry.component';
+import { SanctionComponent } from './entry/ppodetails/sanction/sanction.component';
+import { ManualPpoReceiptComponent } from './manual-ppo-receipt/manual-ppo-receipt.component';
+import { PpoComponent } from './ppo.component';
+
+
+
+const routes: Routes = [
+  {path: '', component: PpoComponent},
+  {path: 'entry', component: EntryComponent},
+  {path: 'manualPpoReceipt', component: ManualPpoReceiptComponent},
+  {path: 'sanction', component: SanctionComponent},
+];
+
+
+@NgModule({
+  declarations: [PpoComponent],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    ReactiveFormsModule,    
+    DynamicTableModule,
+    OptionCardModule,
+    ButtonModule,
+    CommonHeaderModule,
+    DropdownModule,
+    DialogModule,
+    CalendarModule,
+    ReactiveFormsModule,
+    TreasuryDropdownModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class PpoModule { }
