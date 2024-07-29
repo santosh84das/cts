c49c9413da3ca55976f801c60b85d19536172fad
shruti <shruti040112@gmail.com>
2024-07-29 12:38:57 +0530

    some UI changes

---
+++ src/app/core/models/convert-to-family-pension.ts
@@ -0,0 +1,37 @@
+export interface Intr1 {
+    id: string;
+}
+
+export interface Intr2 {
+    Component_description: string;
+    wef: string;
+    amount: number;
+}
+
+export interface Intr3 {
+    id: string;
+}
+
+export interface Intr4 {
+    ppo_id:string;
+    ppo_number:string;
+    pension_name:string;
+    commmencement_date:string;
+    basic_pension:string;
+    bank_name:string;
+    commuted_amount:string;
+    bank_account_no:string;
+    pension_reduced:string;
+    date_of_death:string;
+    pension_status:string;
+    expire_date:string;
+    pension_type:string;
+    category_description:string;
+    remarks:string;
+}
+
+
+
+
+
+
--- src/app/features/pension-dashboard/pensions/pension-process/ppo/convert-to-family-pension/intr.ts
+++
@@ -1,9 +0,0 @@
-export interface Intr1 {
-    id:string;
-}
-
-export interface Intr2 {
-    Component_description:string;
-  wef:string;
-  amount:number;
-}
