1bca1d0174c4352e643316cbb8bddf887c38d9b5
Nilakshi Chakraborty <chakraborty.nilakshi2004@gmail.com>
2024-07-30 17:35:50 +0530

    Added Configuration Options in Status Component

--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstLetterPipe } from 'src/app/core/pipe/first-letter.pipe';
 import { NumberToWordsPipe } from 'src/app/core/pipe/number-to-words.pipe';
+import { PensionerStatusComponent } from './pensioner-status/pensioner-status.component';
 
 @NgModule({
   declarations: [
