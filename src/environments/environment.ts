993271126c25aaafadd9000a58d94cdc0d0199f7
Santosh das <santosh.84das@gmail.com>
2024-08-02 12:44:49 +0530

    ebillingV2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  BaseURL:'http://api.docker.test/api/',
-//   BaseURL:'https://localhost:7249/api/',
+  BaseURL:'http://api.docker.test/api/'
 };
 
 /*
